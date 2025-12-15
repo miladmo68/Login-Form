ffca6115150ead9b4b0a1924e907d25c5e4a3f8b
miladmo68 <miladmo68@gmail.com>
2025-12-15 15:10:44 -0500

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,26 @@ import { useState } from "react";
 
 function App() {
   const [email, setEmail] = useState("");
-  const [email, setEmail] = useState("");
-  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-  return <div></div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Login");
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <input placeholder="Username ..." value={email} className="" />
+        <input placeholder="Username ..." value={email} className="" />
+        <button type="submit" onSubmit={handleSubmit}>
+          Login
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
